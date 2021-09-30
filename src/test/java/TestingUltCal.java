176fa9713c54ec67f888a0cdc7b1896fae33680c
Mino7698 <91067163+Mino7698@users.noreply.github.com>
2021-09-30 18:25:30 +0300

    Zapilil

--- src/test/java/TestingUltCal.java
+++ src/test/java/TestingUltCal.java
@@ -1,6 +1,8 @@
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.assertEquals;
 
@@ -11,66 +13,62 @@ public class TestingUltCal {
     public void setUp() throws Exception {
     }
 
-    @Test //(expected = UltimateCalculator.CalculateException.class)
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    @Test
     public void divisionNegativeExceptionTest() throws UltimateCalculator.CalculateException {
-        UltimateCalculator.EasyCalculate(Double.NEGATIVE_INFINITY, CalculateSymbols.DIVISION, 2);
+        exception.expect(UltimateCalculator.CalculateException.class);
+        assertEquals(5553535,UltimateCalculator.EasyCalculate(Double.NEGATIVE_INFINITY, CalculateSymbols.DIVISION, 02),0.01);
+
     }
 
     @Test
     public void divisionByZeroTest() throws UltimateCalculator.CalculateException {
-        UltimateCalculator.EasyCalculate(100, CalculateSymbols.DIVISION, 0);
+        exception.expect(UltimateCalculator.CalculateException.class);
+        assertEquals(5553535,UltimateCalculator.EasyCalculate(100, CalculateSymbols.DIVISION, 0),0.01);
     }
 
     @Test
     public void divisionPositiveExceptionTest() throws UltimateCalculator.CalculateException {
-        UltimateCalculator.EasyCalculate(Double.POSITIVE_INFINITY, CalculateSymbols.DIVISION, Double.MIN_VALUE);
+        exception.expect(UltimateCalculator.CalculateException.class);
+        assertEquals(5553535,UltimateCalculator.EasyCalculate(Double.POSITIVE_INFINITY, CalculateSymbols.DIVISION, Double.MIN_VALUE),0.01);
     }
 
     @Test
     public void plusPositiveExceptionTest() throws UltimateCalculator.CalculateException {
-        UltimateCalculator.EasyCalculate(Double.MAX_VALUE, CalculateSymbols.PLUS, Double.MAX_VALUE);
+        exception.expect(UltimateCalculator.CalculateException.class);
+        assertEquals(5553535,UltimateCalculator.EasyCalculate(Double.MAX_VALUE, CalculateSymbols.PLUS, Double.MAX_VALUE),0.01);
     }
 
     @Test
     public void plusNegativeExceptionTest() throws UltimateCalculator.CalculateException {
-        UltimateCalculator.EasyCalculate(-Double.MAX_VALUE, CalculateSymbols.PLUS, -Double.MAX_VALUE);
+        exception.expect(UltimateCalculator.CalculateException.class);
+        assertEquals(5553535,UltimateCalculator.EasyCalculate(-Double.MAX_VALUE, CalculateSymbols.PLUS, -Double.MAX_VALUE),0.01);
     }
 
     @Test
     public void minusNegativeExceptionTest() throws UltimateCalculator.CalculateException {
-        UltimateCalculator.EasyCalculate(-Double.MAX_VALUE, CalculateSymbols.MINUS, Double.MAX_VALUE);
+        exception.expect(UltimateCalculator.CalculateException.class);
+        assertEquals(5553535,UltimateCalculator.EasyCalculate(-Double.MAX_VALUE, CalculateSymbols.MINUS, Double.MAX_VALUE),0.01);
     }
 
     @Test
     public void minusPositiveExceptionTest() throws UltimateCalculator.CalculateException {
-        UltimateCalculator.EasyCalculate(Double.MAX_VALUE, CalculateSymbols.MINUS, -Double.MAX_VALUE);
+        exception.expect(UltimateCalculator.CalculateException.class);
+        assertEquals(5553535,UltimateCalculator.EasyCalculate(Double.MAX_VALUE, CalculateSymbols.MINUS, -Double.MAX_VALUE),0.01);
     }
 
     @Test
     public void multiplicationNegativeExceptionTest() throws UltimateCalculator.CalculateException {
-        UltimateCalculator.EasyCalculate(-Double.MAX_VALUE, CalculateSymbols.MULTIPLICATION, Double.MAX_VALUE);
+        exception.expect(UltimateCalculator.CalculateException.class);
+        assertEquals(5553535,UltimateCalculator.EasyCalculate(-Double.MAX_VALUE, CalculateSymbols.MULTIPLICATION, Double.MAX_VALUE),0.01);
     }
 
     @Test
     public void multiplicationPositiveExceptionTest() throws UltimateCalculator.CalculateException {
-        UltimateCalculator.EasyCalculate(Double.MAX_VALUE, CalculateSymbols.MULTIPLICATION, Double.MAX_VALUE);
+        exception.expect(UltimateCalculator.CalculateException.class);
+        assertEquals(5553535,UltimateCalculator.EasyCalculate(Double.MAX_VALUE, CalculateSymbols.MULTIPLICATION, Double.MAX_VALUE),0.01);
     }
 
 }
-
-        /*
-          assertEquals(25.0, UltimateCalculator.EasyCalculate(5.0, CalculateSymbols.MULTIPLICATION, 5.0), 0.001);
-        assertEquals(28.0, UltimateCalculator.EasyCalculate(7.0, CalculateSymbols.MULTIPLICATION, 4.0), 0.001);
-        assertEquals(5.562684646268003E-309, UltimateCalculator.EasyCalculate(1.0, CalculateSymbols.DIVISION, Double.MAX_VALUE), 0.001);
-        assertEquals(1, UltimateCalculator.EasyCalculate(Double.MAX_VALUE, CalculateSymbols.DIVISION, Double.MAX_VALUE), 0.001);
-        assertEquals(Double.POSITIVE_INFINITY, UltimateCalculator.EasyCalculate(Double.MAX_VALUE, CalculateSymbols.DIVISION, Double.MIN_VALUE), 0.001);
-        assertEquals(4.9E-324, UltimateCalculator.EasyCalculate(Double.MIN_VALUE, CalculateSymbols.DIVISION, Double.MAX_VALUE), 0.001);
-        assertEquals(1.0, UltimateCalculator.EasyCalculate(Double.MAX_VALUE, CalculateSymbols.DIVISION, Double.MAX_VALUE), 0.001);
-        assertEquals(5.0, UltimateCalculator.EasyCalculate(5.0, "/", 0.0), 0.001);
-        assertEquals(Double.NEGATIVE_INFINITY, UltimateCalculator.EasyCalculate(-(Double.MAX_VALUE), CalculateSymbols.MINUS, Double.MAX_VALUE), 0.001);
-
-        for (int ik = -3; ik < 3; ik++) {
-            for (int jk = 3; jk > -3; jk--) {
-                assertEquals((ik * 1E300) / (jk * 1E300), UltimateCalculator.EasyCalculate(ik * 1E300, CalculateSymbols.DIVISION, jk * 1E300), 0.001);
-            }
-        }*/
